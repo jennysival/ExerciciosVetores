49041ec93d7b13916f5e1492123dc4a0556dac35
jennysival <jeniffersival@gmail.com>
2022-04-01 16:55:53 -0300

    adicionar exercicio 4

---
+++ src/Exercicio4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Quantos itens terá na sua lista de compras?");
+        int qtdItens = leitor.nextInt();
+
+        String[] listaCompras = new String[qtdItens];
+
+        for(int i = 0; i < listaCompras.length; i++){
+            int Lista = i + 1;
+            System.out.println("Digite o item "+Lista+":");
+            listaCompras[i] = leitor.next();
+        }
+
+        System.out.println("-------------------------------");
+        System.out.println("Aqui está sua lista de compras:");
+        System.out.println("-------------------------------");
+        
+        for(int i = 0; i < listaCompras.length; i++){
+            System.out.println(listaCompras[i]);
+        }
+
+    }
+}
